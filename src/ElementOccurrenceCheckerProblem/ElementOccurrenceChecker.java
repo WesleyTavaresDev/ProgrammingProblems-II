607f0f6a3a1cfbd5f399eb95055a6f2e8fdc29af
WesleyTavDev <91393560+WesleyTavaresGameDev@users.noreply.github.com>
2022-02-02 22:02:49 -0300

    PROBLEM: * .Write a function that checks whether an element occurs in a list. *
    Reason for println and don't printf use:
    How I only need to concatenate one integer on my text, use printf, on my eyes, is not necessary.

---
+++ src/ElementOccurrenceCheckerProblem/ElementOccurrenceChecker.java
@@ -0,0 +1,22 @@
+package ElementOccurrenceCheckerProblem;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ElementOccurrenceChecker
+{
+    public static void main(String[] args)
+    {
+        List<Integer> numberList = Arrays.asList(5, 4, 6, 9, 8, 10);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, enter a number -> ");
+        int guess = scanner.nextInt();
+
+        if(numberList.contains(guess))
+            System.out.println("Our number list contains the number " + guess);
+        else
+            System.out.println("Our number list doesn't contains the number " + guess);
+    }
+}
