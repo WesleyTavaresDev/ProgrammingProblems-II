aff7c3f4dd0206919caf73583835401af92e81c6
WesleyTavDev <91393560+WesleyTavaresGameDev@users.noreply.github.com>
2022-02-02 21:24:28 -0300

    PROBLEM: * 2.Write function that reverses a list, preferably in place. *

---
+++ src/ListReverserProblem/ListReverser.java
@@ -0,0 +1,26 @@
+package ListReverserProblem;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListReverser
+{
+    public static void main(String[] args)
+    {
+        List<Integer> numberList = Arrays.asList(6, 13, 5, 10, 40);
+
+        System.out.println("Reversed with Stream ->");
+        System.out.println(streamReverseList(numberList));
+    }
+
+    public static List<Integer> streamReverseList(List<Integer> numberList)
+    {
+        return  numberList
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+}
