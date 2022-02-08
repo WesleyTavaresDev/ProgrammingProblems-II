87408240d7d08653ccc1adafc74c4ea18703dd77
WesleyTavDev <91393560+WesleyTavaresGameDev@users.noreply.github.com>
2022-02-08 06:38:33 -0300

    OddPositions ->   Replace loop with pipeline -> for loop replaced with pipeline.

--- src/com/element/odd/OddPositions.java
+++ src/com/element/odd/OddPositions.java
@@ -2,7 +2,6 @@ package com.element.odd;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class OddPositions
 {
@@ -10,11 +9,9 @@ public class OddPositions
     {
         List<Integer> numberList = Arrays.asList(5, 13, 40, 41, 6, 8);
 
-        for(int i = 0; i < numberList.size(); i++)
-        {
-            if(i % 2 == 0)
-                continue;
-            System.out.println(numberList.get(i));
-        }
+        numberList
+                .stream()
+                .filter(o -> o % 2 != 0)
+                .forEach(System.out::println);
     }
 }
